bb520ebea3d6de684e5f48505309ffcfaf71857b
Paolo Patierno <ppatierno@live.com>
2018-12-25 10:15:19 +0100

    Added greceful shutdown to the producer

--- producer/main.go
+++ producer/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/ppatierno/kafka-go-examples/util"
@@ -11,6 +14,18 @@ import (
 )
 
 func main() {
+
+	signals := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGKILL)
+
+	// go routine for getting signals asynchronously
+	go func() {
+		sig := <-signals
+		fmt.Println(sig)
+		done <- true
+	}()
 
 	bootstrapServers := strings.Split(util.GetEnv(util.BootstrapServers, "localhost:9092"), ",")
 	topic := util.GetEnv(util.Topic, "my-topic")
@@ -23,6 +38,7 @@ func main() {
 
 	i := 1
 
+	var closing bool
 	for {
 		message := fmt.Sprintf("Message-%d", i)
 		err := w.WriteMessages(context.Background(), kafka.Message{Value: []byte(message)})
@@ -30,8 +46,20 @@ func main() {
 			fmt.Println("Sent message: ", message)
 		}
 		i++
-		time.Sleep(time.Second)
+
+		// checking close or sleep
+		select {
+		case closing = <-done:
+			fmt.Println("closing =", closing)
+		default:
+			time.Sleep(time.Second)
+		}
+
+		if closing {
+			break
+		}
 	}
 
+	fmt.Println("Closing")
 	w.Close()
 }
